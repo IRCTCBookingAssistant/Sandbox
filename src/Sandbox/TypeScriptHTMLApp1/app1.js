70a342ca9ef15d7dc47a44530ed8279737a80590
IRCTCBookingAssistant <IRCTCBookingAssistant@gmail.com>
2014-07-20 07:11:08 +0530

    improved debugging

--- src/Sandbox/TypeScriptHTMLApp1/app1.js
+++ src/Sandbox/TypeScriptHTMLApp1/app1.js
@@ -1,1 +1,27 @@
-﻿
+﻿; (function (window, document, undefined) {
+    'use strict';
+    var Greeter = (function () {
+        function Greeter(message) {
+            var msg = message;
+            this.sayHello = function () {
+                return "Hi " + message + "<br/>";
+            };
+        }
+        return Greeter;
+    })();
+
+    window.onload = function () {
+        var log = document.getElementById('content');
+        var greeter = new Greeter("Anil");
+        log.innerHTML += greeter.sayHello();
+        var greeter2 = new Greeter("Sunil");
+        log.innerHTML += greeter2.sayHello();
+        log.innerHTML += greeter.sayHello();
+        var el = document.getElementById('btnStart');
+        el.addEventListener('click', function () {
+            log.innerHTML += greeter.sayHello();
+            log.innerHTML += greeter2.sayHello();
+            log.innerHTML += greeter.sayHello();
+        });
+    };
+})(window, document);
